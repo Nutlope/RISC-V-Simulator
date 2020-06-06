73dd490a9c2f02f89bb2497154f6b9c07e9c1fcf
shihao-song <shihao.song@drexel.edu>
2020-06-06 19:46:02 -0400

    keep working on the course projects

--- project_2_3_4_5/Core.h
+++ project_2_3_4_5/Core.h
@@ -6,8 +6,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #define BOOL bool
+
+typedef uint8_t Byte;
+typedef int64_t Signal;
+typedef int64_t Register;
 
 struct Core;
 typedef struct Core Core;
@@ -16,19 +21,59 @@ typedef struct Core
     Tick clk; // Keep track of core clock
     Addr PC; // Keep track of program counter
 
-    // TODO, define your components here
     // What else you need? Data memory? Register file?
     Instruction_Memory *instr_mem;
    
-    uint8_t data_mem[1024]; // 1KB of data memory
+    Byte data_mem[1024]; // data memory
 
-    int64_t reg_file[32]; // register file.
+    Register reg_file[32]; // register file.
 
-    // TODO, simulation function
     bool (*tick)(Core *core);
 }Core;
 
 Core *initCore(Instruction_Memory *i_mem);
 bool tickFunc(Core *core);
+
+// FIXME. Implement the following functions
+// FIXME (1). Control Unit.
+typedef struct ControlSignals
+{
+    Signal Branch;
+    Signal MemRead;
+    Signal MemtoReg;
+    Signal ALUOp;
+    Signal MemWrite;
+    Signal ALUSrc;
+    Signal RegWrite;
+}ControlSignals;
+void ControlUnit(Signal input,
+                 ControlSignals *signals);
+
+// FIXME (2). ALU Control Unit.
+Signal ALUControlUnit(Signal ALUOp,
+                      Signal Funct7,
+                      Signal Funct3);
+
+// FIXME (3). Imme. Generator
+Signal ImmeGen(Signal input);
+
+// FIXME (4). ALU
+Signal ALU(Signal input_0,
+           Signal input_1,
+           Signal ALU_ctrl_signal,
+           Signal ALU_result,
+           Signal zero);
+
+// (4). MUX
+Signal MUM(Signal sel,
+           Signal input_0,
+           Signal input_1);
+
+// (5). Add
+Signal Add(Signal input_0,
+           Signal input_1);
+
+// (6). ShiftLeft1
+Signal ShiftLeft1(Signal input);
 
 #endif
